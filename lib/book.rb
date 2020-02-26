5659e9c05788c666c9879cc193d7e825e6f20fec
John J Marshall Jr <johnjamesmarshalljr@gmail.com>
2020-02-26 15:21:40 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,3 +1,5 @@
+
+
 class Book
   attr_accessor :author, :page_count, :genre
   attr_reader :title
@@ -6,12 +8,8 @@ class Book
     @title = title
   end
 
-  def title
-    @title
-  end
-
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
- 
+
 end
